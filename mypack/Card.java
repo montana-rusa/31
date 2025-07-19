e79e21eceedbd536ac76e47602a97bf0e62cd095
Emma <emlcliffe@gmail.com>
2025-07-19 19:30:39 +0100

    adding classes

---
+++ mypack/Card.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Card {
+
+    public String suit;
+    public String rank;
+
+    public Card(String suit, String value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
---
+++ mypack/Game.java
@@ -0,0 +1,14 @@
+package mypack;
+
+import java.util.*;
+
+public class Game {
+
+
+    String[] suit = {"♠️", "♣️","♦️","♥️"}
+    List<Card> deck = new ArrayList<>();
+
+
+
+    
+}
