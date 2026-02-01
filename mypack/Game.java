bed8a0625a60d3f2c3f2bccc05d9ddd3add5581f
Emma <emlcliffe@gmail.com>
2026-02-01 22:19:57 +0000

    card dealing functionality added

--- mypack/Game.java
+++ mypack/Game.java
@@ -2,18 +2,44 @@ package mypack;
 
 public class Game {
 
+    // Deal initial cards to player and computers
+    private static Card[] dealCards(Deck deck, int count) {
+        Card[] cards = new Card[count];
+        for (int i = 0; i < count; i++) {
+            cards[i] = deck.cards.remove(0);
+        }
+        return cards; }
+
     void startGame() {
         Deck deck = new Deck();
 
-        // Deal initial cards to player and computer
-        Card[] playerCards = {deck.cards.remove(0), deck.cards.remove(0), deck.cards.remove(0)};
-        Card[] comCards = {deck.cards.remove(0), deck.cards.remove(0), deck.cards.remove(0)};
+        Player player = new Player(dealCards(deck, 3));
+        Com com1 = new Com(dealCards(deck, 3));
+        Com com2 = new Com(dealCards(deck, 3));
+        Com com3 = new Com(dealCards(deck, 3));
+        Card[] middle = dealCards(deck, 3);
 
-        Player player = new Player(playerCards);
-        Com com = new Com(comCards);
-
+        // Display hands
         System.out.println("Player's Hand:");
         player.printHand();
+
+        System.out.println("z\nComputer 1's Hand:");
+        com1.printHand();
+
+        System.out.println("\nComputer 2's Hand:");
+        com2.printHand();
+
+        System.out.println("\nComputer 3's Hand:");
+        com3.printHand();
+
+        System.out.println("\nMiddle Cards:");
+        for (Card card : middle) {
+            System.out.println(card.rank + " of " + card.suit);
+        }   
+
+
+
+
     }
 
 
---
+++ mypack/Main.java
@@ -0,0 +1,12 @@
+package mypack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
+    }
+
+
+    
+}
